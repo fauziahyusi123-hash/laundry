ea6aedf34dbd7088968b8a108646d15fd98c3598
fauziahyusi123-hash <fauziahyusi123@gmail.com>
2025-11-28 11:51:05 +0700

    membenarkan erorr

--- admin/header.php
+++ admin/header.php
@@ -1,66 +1,46 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>Sistem Informasi Laundry</title>
-    <!-- pastikan path folder 'assets' sesuai di projectmu -->
-    <link rel="stylesheet" href="../assts/css/bootstrap.min.css">
-    <script src="../assts/js/jquery.min.js"></script>
-    <script src="../assts/js/bootstrap.min.js"></script>
-    <style>
-        body { background: #f0f0f0; }
-    </style>
+	<title>Sistem Informasi Laundry</title>
+	<link rel="stylesheet" type="text/css" href="../assts/css/bootstrap.css">
+	<script type="text/javascript" src="../assts/js/jquery.js"></script>
+	<script type="text/javascript" src="../assts/js/bootstrap.js"></script>
+
 </head>
-<body>
-<?php
-session_start();
-// cek apakah session 'status' diset dan bernilai "login"
-if (!isset($_SESSION['ststus']) || $_SESSION['ststus'] !== "login") {
-    header("Location: ../index.php?pesan=belum_login");
-    exit;
-}
-?>
-<nav class="navbar navbar-inverse" style="border-radius: 0;">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <!-- tombol toggle untuk mobile -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-navbar" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="index.php">LAUNDRY</a>
-        </div>
+<body style="background: #f0f0f0;">
+	<?php
+	session_start();
+	if ($_SESSION['ststus']!="login"){
+		header("localtion:../index.php?pesan=belum_login");
+	}
+	?>
 
-        <div class="collapse navbar-collapse" id="main-navbar">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="index.php"><i class="glyphicon glyphicon-home"></i> Dashboard</a></li>
-                <li><a href="pelanggan.php"><i class="glyphicon glyphicon-user"></i> Pelanggan</a></li>
-                <li><a href="transaksi.php"><i class="glyphicon glyphicon-random"></i> Transaksi</a></li>
-                <li><a href="laporan.php"><i class="glyphicon glyphicon-list-alt"></i> Laporan</a></li>
+	<nav class="navbar navbar-inverse" style="border-radius: 0px;">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="index.php">LAUNDRY</a>
+			</div>
+			<div class="collapse navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="index.php"><i class="glyphicon glyphicon-home"></i>Dashboard</a></li>
+					<li class="active"><a href="pelanggan.php"><i class="glyphicon glyphicon-user"></i>Pelanggan</a></li>
+					<li class="active"><a href="transaksi.php"><i class="glyphicon glyphicon-random"></i>transasksi</a></li>
+					<li class="active"><a href="laporan.php"><i class="glyphicon glyphicon-list-alt"></i>Laporan</a></li>
 
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                        <i class="glyphicon glyphicon-wrench"></i> Pengaturan <span class="caret"></span>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a href="harga.php"><i class="glyphicon glyphicon-usd"></i> Pengaturan Harga</a></li>
-                        <li><a href="ganti_password.php"><i class="glyphicon glyphicon-lock"></i> Ganti Password</a></li>
-                    </ul>
-                </li>
-
-                <li><a href="logout.php"><i class="glyphicon glyphicon-log-out"></i> Log Out</a></li>
-            </ul>
-
-            <ul class="nav navbar-nav navbar-right">
-                <li><p class="navbar-text">Halo, <b><?php echo htmlspecialchars($_SESSION['username'] ?? ''); ?></b>!</p></li>
-            </ul>
-        </div>
-    </div>
-</nav>
-
-<!-- sisa body... -->
-
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aris-expanded="false"><i class="glyphicon glyphicon-wrench"></i>Pengaturan<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="harga.php"><i class="glyphicon glyphicon-usd"></i>Pengaturan Harga</a></li>
+							<li><a href="ganti_password.php"><i class="glyphicon glyphicon-lock"></i>Ganti Password</a></li>
+						</ul>
+					</li>
+					<li><a href="logout.php"><i class="glyphicon glyphicon-log-out"></i>Log Out</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><p class="navbar-text">Halo, <b><?php echo $_SESSION['username']; ?></b>!</p></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 </body>
 </html>
